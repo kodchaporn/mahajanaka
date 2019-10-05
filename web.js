eb87335c1fbdbe29b798ff5a3b49e3ae23f6fa9f
Kodchaporn Satapanasatien <60070001@kmitl.ac.th>
2019-10-05 13:02:18 +0700

    yeah

---
+++ web.js
@@ -0,0 +1,52 @@
+function toHomepage(){
+	clearClick()
+	homepage.classList.add("home");
+	content.style.zIndex = -3;
+}
+
+document.getElementById("m_story").onclick = function() {clickStory()};
+
+function clickStory() {
+	clearClick()
+	homepage.classList.add("story");
+	m_story.classList.add("clickMenu");
+	content.style.zIndex = 4;
+}
+
+document.getElementById("m_history").onclick = function() {clickHistory()};
+
+function clickHistory() {
+	clearClick()
+	homepage.classList.add("history");
+	m_history.classList.add("clickMenu");
+	content.style.zIndex = 4;
+}
+
+document.getElementById("m_media").onclick = function() {clickMedia()};
+
+function clickMedia() {
+	clearClick()
+	homepage.classList.add("media");
+	m_media.classList.add("clickMenu");
+	content.style.zIndex = 4;
+}
+
+document.getElementById("m_artist").onclick = function() {clickArtist()};
+
+function clickArtist() {
+	clearClick()
+	homepage.classList.add("artist");
+	m_artist.classList.add("clickMenu");
+	content.style.zIndex = 4;
+}
+
+function clearClick(){
+	m_story.classList.remove("clickMenu");
+	m_history.classList.remove("clickMenu");
+	m_media.classList.remove("clickMenu");
+	m_artist.classList.remove("clickMenu");
+	homepage.classList.remove("story");
+	homepage.classList.remove("history");
+	homepage.classList.remove("media");
+	homepage.classList.remove("artist");
+}
