fb0afab3f10eab88f363ee4ac09849f8d29236e3
bpthn <60070032@kmitl.ac.th>
2019-10-15 09:43:06 +0700

    omg

--- home.js
+++ home.js
@@ -9,6 +9,7 @@ document.getElementById("m_story").onclick = function() {clickStory()};
 
 function clickStory() {
 	clearClick()
+	caption.innerText = "เรื่องย่อพระราชนิพนธ์พระมหาชนก";
 	homepage.classList.add("story");
 	obj.classList.add("obj-s");
 	obj.classList.add("obj-scale");
@@ -20,7 +21,8 @@ function clickStory() {
 document.getElementById("m_history").onclick = function() {clickHistory()};
 
 function clickHistory() {
-	clearClick()
+	clearClick();
+	caption.innerText = "ประวัติความเป็นมาของพระราชนิพนธ์พระมหาชนก และผลงานการประพันธ์ของพระบาทสมเด็จพระปรมินทรมหาภูมิพลอดุลเดช";
 	homepage.classList.add("history");
 	obj.classList.add("obj-h");
 	obj.classList.add("obj-scale");
@@ -34,6 +36,7 @@ document.getElementById("m_media").onclick = function() {clickMedia()};
 
 function clickMedia() {
 	clearClick()
+	caption.innerText = "ผลงานฉบับตีพิมพ์ ผลงานการ์ตูน และ แอนิเมชั่น";
 	homepage.classList.add("media");
 	obj.classList.add("obj-m");
 	obj.classList.add("obj-scale");
@@ -46,6 +49,7 @@ document.getElementById("m_artist").onclick = function() {clickArtist()};
 
 function clickArtist() {
 	clearClick()
+	caption.innerText = "คลังภาพรวบรวมศิลปินที่ถวายผลงานภาพประกอบพระราชนิพนธ์พระมหาชนก";
 	homepage.classList.add("artist");
 	obj.classList.add("obj-a");
 	obj.classList.add("obj-scale");
@@ -55,6 +59,7 @@ function clickArtist() {
 }
 
 function clearClick(){
+	caption.innerText = "";
 	obj.classList.remove("obj-scale");
 	homepage.classList.remove("home-1");
 	obj.classList.remove("home-2");
