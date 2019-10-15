d1cc9def046fabf21060de90b6ee1c750574555a
bpthn <60070032@kmitl.ac.th>
2019-10-15 11:39:42 +0700

    Update responsive.js

--- responsive.js
+++ responsive.js
@@ -1,12 +1,16 @@
-window.onload = checkWidth();
+//window.onload = checkWidth();
+window.addEventListener('resize', checkWidth);
 function checkWidth(){
+	console.log('gg')
 	let chkWidth = document.body.getBoundingClientRect();
 	if (chkWidth.width < 420) {
 		myTopnav.classList.add("topnav");
+		// myTopnav.style.display = "block";
 		// logo.outerHTML = "";
 	}
 	else{
-		myTopnav.outerHTML = "";
+		myTopnav.classList.remove("topnav")
+		// myTopnav.style.display = "none";
 		logo.src = `img/home/logo.svg`;
 	}
 }
